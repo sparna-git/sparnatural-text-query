3e02eb0ec4f04f7aa644fe49c1ce7f88c749dee8
BouzelatTarek <106556135+BouzelatTarek@users.noreply.github.com>
2025-09-09 09:53:51 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,6 @@ module.exports = {
     "query-micro": ["./src/sparnatural-text-query/lit/query-micro.ts"],
     "query-send": ["./src/sparnatural-text-query/lit/query-send.ts"],
     "query-dropdown": ["./src/sparnatural-text-query/lit/query-dropdown.ts"],
-    "service-container": ["./src/services/Lit/service-container.ts"],
   },
 
   output: {
